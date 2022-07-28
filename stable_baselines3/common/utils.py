2b2368c9bc1ba36cd571d123179c486cde544e85
Aru <rm.arumugam.2000@gmail.com>
2022-07-28 16:18:17 +0800

    replace valueerror with warning

--- stable_baselines3/common/utils.py
+++ stable_baselines3/common/utils.py
@@ -224,7 +224,7 @@ def check_for_correct_spaces(env: GymEnv, observation_space: gym.spaces.Space, a
     :param action_space: Action space to check against
     """
     if observation_space != env.observation_space:
-        raise ValueError(f"Observation spaces do not match: {observation_space} != {env.observation_space}")
+        print(f"WARNING: Observation spaces do not match: {observation_space} != {env.observation_space}")
     if action_space != env.action_space:
         raise ValueError(f"Action spaces do not match: {action_space} != {env.action_space}")
 
